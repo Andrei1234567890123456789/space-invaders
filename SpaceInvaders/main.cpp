25bd92f9ff32c553b61ca5df16efce1f5c2b6649
ahbodya <bogdanrusakovr@gmail.com>
2025-05-13 18:50:09 +0300

    Promenih /*game.run();*/               game.runManualInput();

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -4,7 +4,7 @@ int main(){
     /* If you have problems with the button press reading system, 
     use game.runManualInput() instead of game.run().Then you will be 
     able to type the inputs through the console using std::cin */
-    game.run();
-    // game.runManualInput();
+    /*game.run();*/
+     game.runManualInput();
     return 0;
 }
